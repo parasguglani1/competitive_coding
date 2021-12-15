9b820f1477c4561ffc2e1becf21b9bbc1b871e01
parasguglani1 <73358562+parasguglani1@users.noreply.github.com>
2021-12-15 21:33:21 +0530

    Update equal_beauty.cpp

--- Codechef/Snackdown/1A/equal_beauty.cpp
+++ Codechef/Snackdown/1A/equal_beauty.cpp
@@ -1,90 +1,54 @@
+#include<iostream>
 #include <bits/stdc++.h>
+
 using namespace std;
+#define ll long long
 
-#define ff first
-#define ss second
-#define int long long
-#define setbits(x) __builtin_popcountll(x)
-#define zrobits(x) __builtin_ctzll(x)
-#define mod 1000000007
-#define inf 1e18
-#define PI 3.1415926535897932384626
-#define ps(x, y) fixed << setprecision(y) << x
-#define w(x)  \
-    int x;    \
-    cin >> x; \
-    while (x--)
-#define sortall(x) sort(all(x))
-#define rep(i, n) for (int i = 0; i < n; ++i)
-#define REP(i, k, n) for (int i = k; i <= n; ++i)
-#define REPR(i, k, n) for (int i = k; i >= n; --i)
-void paras()
+int main()
 {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-#ifndef ONLINE_JUDGE
-    freopen("D:\\Programming\\ds\\Codechef\\input.txt", "r", stdin);
-    freopen("D:\\Programming\\ds\\Codechef\\output.txt", "w", stdout);
-#endif
-}
-
-int32_t main()
-
-{
-    paras();
-    w(t)
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    cin>>t;
+    while(t--)
     {
-        int n, sp2, sp1;
-        int minimum = INT_MAX;
+        ll n, ans1 = 0, ans2 = 0, d2, d1;
+        ll minimum = INT_MAX;
 
         cin >> n;
 
-        vector<int> A(n);
-        rep(i,n)
-        { cin >> A[i];}
+        vector<ll> a(n);
+        for(ll i=0;i<n;i++)
+        { cin >> a[i];}
+        sort(a.begin(), a.end());
 
-        sort(A.begin(), A.end());
-
-        int left = 1, right = n - 2;
+        ll l = 1, r = n - 2;
         if (n == 2)
         {
             cout << 0 << endl;
             continue;
         }
-
         else if (n == 3)
         {
-            cout << min(A[1] - A[0], A[2] - A[1]) << endl;
+            cout << min(a[1] - a[0], a[2] - a[1]) << endl;
             continue;
         }
+         for(ll i=0;i<n-1;i++)
+        {ans1 += abs(a[i] - a[(n - 1) / 2]);} 
+        for (ll i = 1; i < n; i++)
+            ans2 += abs(a[i] - a[1 + (n - 1) / 2]);
+        minimum = min(ans1, ans2);
 
-        int ans1 = 0, ans2 = 0;
-        rep(i,n-1)
+        while (l < r)
         {
-            ans1 = ans1+ abs(A[i] - A[(n - 1) / 2]);
-            
-            }
-
-      for (int i = 1; i < n; i++)
-        {
-            ans2 = ans2+ abs(A[i] - A[1 + (n - 1) / 2]);}
-
-            minimum = min(ans1, ans2);
-
-        while (left < right)
-        {
-            sp2 = A[n - 1] - A[left], sp1 = A[right] - A[0];
-
-            minimum = min(minimum, abs(sp1 - sp2));
-            if (sp1 < sp2)
-                left++;
+            d2 = a[n - 1] - a[l], d1 = a[r] - a[0];
+            minimum = min(minimum, abs(d1 - d2));
+            if (d1 < d2)
+                l++;
             else
-                right--;
+                r--;
         }
-    
-        cout << minimum << endl;       
-    
+        cout << minimum << endl;
     }
     return 0;
 }
