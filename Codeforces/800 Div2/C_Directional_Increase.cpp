2c4bd00afb0336f486935e512589f30852eeacdb
paras <parasguglani1@gmail.com>
2022-06-17 07:02:02 +0530

    codeforces 800

---
+++ Codeforces/800 Div2/C_Directional_Increase.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+using namespace std;
+using namespace chrono;
+
+#define ff first
+#define ss second
+#define int long long
+using ll = long long;
+#define setbits(x) __builtin_popcountll(x)
+#define zrobits(x) __builtin_ctzll(x)
+#define mod 1000000007
+#define inf 1e18
+#define PI 3.1415926535897932384626
+#define ps(x, y) fixed << setprecision(y) << x
+#define w(x)  \
+    int x;    \
+    cin >> x; \
+    while (x--)
+#define all(x) (x).begin(), (x).end()
+#define sortall(x) sort(all(x))
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define REP(i, k, n) for (int i = k; i < n; ++i)
+#define REPR(i, k, n) for (int i = k; i > n; --i)
+#define pb push_back
+#define mp make_pair
+#define pii pair<int, int>
+#define vi vector<int>
+void pgsolve()
+{
+    int size;
+    cin >> size;
+    vi v(size);
+    int sumall = 0;
+    rep(i, size)
+    {
+        cin >> v[i];
+        sumall =sumall+ v[i];
+    }
+    if (v[0] < 0 || v[size - 1] > 0 || sumall != 0)
+    {
+        cout << "No" << endl;
+        return;
+    }
+    sumall = v[size - 1];
+    REPR(i, size - 2, 0)
+    {
+        if (v[i] == 0)
+        {
+            continue;
+        }
+        sumall =sumall+ v[i];
+        if (sumall >= 0)
+        {
+            cout << "No" << endl;
+            return;
+        }
+    }
+    cout << "Yes" << endl;
+}
+
+int32_t main()
+
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    auto start1 = high_resolution_clock::now();
+
+#ifdef __GNUC__
+    freopen("Error.txt", "w", stderr);
+#endif
+
+    w(t)
+    {
+        pgsolve();
+    }
+    auto stop1 = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifdef __GNUC__
+    cerr << "\n Time: " << duration.count() / 1000 << " ms" << endl;
+#endif
+    return 0;
+}
