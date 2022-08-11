b97bcdf66c2518c3032058e64ca1486abcd16bc6
paras <parasguglani1@gmail.com>
2022-08-11 19:04:49 +0530

    make it increasing

--- Codeforces/787 div3/B_Make_It_Increasing.cpp
+++ Codeforces/787 div3/B_Make_It_Increasing.cpp
@@ -28,15 +28,32 @@ using ll = long long;
 #define vi vector<int>
 void pgsolve()
 {
-    int n;
-    cin>>n;
-    int a[n];
-    rep(i,n)
-    {
-        cin>>a[i];
-    }
-    
 
+/*     We will process the elements of the sequence starting from the end of the sequence. Each element ai (1≤i≤n−1) will be divided by 2 until it is less than ai+1. If at some point it turns out that ai+1=0, it is impossible to obtain the desired sequence.
+ */
+
+    int n;
+    cin >> n;
+    int a[n];
+    rep(i, n)
+    {
+        cin >> a[i];
+    }
+    int count=0;
+    for (int i = n - 1; i >= 0; i--)
+    {
+        while(a[i]>=a[i+1] & a[i]>0)
+        {
+            a[i]/=2;
+            count++;
+        }
+        if(a[i]==a[i+1])
+        {
+            cout<<-1<<endl;
+            return;
+        }
+    }
+    cout<<count<<endl;
 }
 
 int32_t main()
@@ -48,7 +65,7 @@ int32_t main()
     auto start1 = high_resolution_clock::now();
 
 #ifdef __GNUC__
-	freopen("Error.txt", "w", stderr);
+    freopen("Error.txt", "w", stderr);
 #endif
 
     w(t)
