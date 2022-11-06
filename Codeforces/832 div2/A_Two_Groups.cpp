05e2abd6f2fb35f95905820aaf64f7d52f983aa2
paras <parasguglani1@gmail.com>
2022-11-06 20:05:18 +0530

    832 div2

---
+++ Codeforces/832 div2/A_Two_Groups.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+using namespace chrono;
+
+#define ff first
+#define ss second
+#define int long long
+using ll = long long;
+#define setbits(x) __builtin_popcountll(x)
+#define zrobits(x) __builtin_ctzll(x)
+#define mod 1000000007
+#define inf 1e18
+#define PI 3.1415926535897932384626
+#define ps(x, y) fixed << setprecision(y) << x
+#define w(x)  \
+    int x;    \
+    cin >> x; \
+    while (x--)
+#define all(x) (x).begin(), (x).end()
+#define sortall(x) sort(all(x))
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define REP(i, k, n) for (int i = k; i < n; ++i)
+#define REPR(i, k, n) for (int i = k; i > n; --i)
+#define pb push_back
+#define mp make_pair
+#define sz(v) (int)v.size()
+#define pii pair<int, int>
+#define vi vector<int>
+void pgsolve()
+{
+    int n;
+    cin >> n;
+    int a[n];
+    rep(i, n) cin >> a[i];
+    int sum1 = 0;
+    int sum2 = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] > 0)
+            sum1 += a[i];
+        else
+            sum2 += a[i];
+    }
+    sum1 = abs(sum1);
+    sum2 = abs(sum2);
+
+    cout << abs(sum1 - sum2) << endl;
+}
+
+int32_t main()
+
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    auto start1 = high_resolution_clock::now();
+
+#ifdef __GNUC__
+    freopen("Error.txt", "w", stderr);
+#endif
+
+    w(t)
+    {
+        pgsolve();
+    }
+    auto stop1 = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifdef __GNUC__
+    cerr << "\n Time: " << duration.count() / 1000 << " ms" << endl;
+#endif
+    return 0;
+}
