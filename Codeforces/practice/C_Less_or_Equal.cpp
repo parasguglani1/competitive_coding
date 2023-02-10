586ad712502c680716aea9de7a4febf2ef92971d
paras <parasguglani1@gmail.com>
2023-02-10 14:07:26 +0530

    practice

---
+++ Codeforces/practice/C_Less_or_Equal.cpp
@@ -0,0 +1,95 @@
+#include <bits/stdc++.h>
+using namespace std;
+using namespace chrono;
+
+#define ff first
+#define ss second
+#define int long long
+using ll = long long;
+#define setbits(x) __builtin_popcountll(x)
+#define zrobits(x) __builtin_ctzll(x)
+#define mod 1000000007
+#define inf 1e18
+#define PI 3.1415926535897932384626
+#define ps(x, y) fixed << setprecision(y) << x
+#define w(x)  \
+    int x;    \
+    cin >> x; \
+    while (x--)
+#define all(x) (x).begin(), (x).end()
+#define sortall(x) sort(all(x))
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define REP(i, k, n) for (int i = k; i < n; ++i)
+#define REPR(i, k, n) for (int i = k; i > n; --i)
+#define pb push_back
+#define mp make_pair
+#define sz(v) (int)v.size()
+#define pii pair<int, int>
+#define vi vector<int>
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+void pgsolve()
+{
+    int n, k;
+    cin >> n >> k;
+    int a[n];
+    map<int, int> m;
+    rep(i, n)
+    {
+        cin >> a[i];
+        m[a[i]]++;
+    }
+    sort(a, a + n);
+    rep(i, n)
+    {
+        // cout << a[i] << " ";
+    }
+
+    int ans;
+
+    if (k == 0)
+    {
+        ans = a[0] - 1;
+    }
+    else
+    {
+        ans = a[k - 1];
+    }
+
+    int cnt = 0;
+
+    for (int i = 0; i < n; ++i)
+        if (a[i] <= ans)
+            ++cnt;
+
+    if (cnt != k || !(1 <= ans && ans <= 1000 * 1000 * 1000))
+    {
+        cout << ("-1");
+        return;
+    }
+    cout << ans << endl;
+}
+
+int32_t main()
+
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    auto start1 = high_resolution_clock::now();
+
+#ifdef __GNUC__
+    freopen("Error.txt", "w", stderr);
+#endif
+
+    // w(t)
+    {
+        pgsolve();
+    }
+    auto stop1 = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifdef __GNUC__
+    cerr << "\n Time: " << duration.count() / 1000 << " ms" << endl;
+#endif
+    return 0;
+}
