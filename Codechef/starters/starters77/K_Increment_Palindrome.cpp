873d0fd5ec920eacfe7f6d9f7474b92c4f849346
paras <parasguglani1@gmail.com>
2023-02-18 00:15:34 +0530

    143 div2 and starters 77

---
+++ Codechef/starters/starters77/K_Increment_Palindrome.cpp
@@ -0,0 +1,104 @@
+#include <bits/stdc++.h>
+using namespace std;
+using namespace chrono;
+
+#define ff first
+#define ss second
+#define int long long
+using ll = long long;
+#define setbits(x) __builtin_popcountll(x)
+#define zrobits(x) __builtin_ctzll(x)
+#define mod 1000000007
+#define inf 1e18
+#define PI 3.1415926535897932384626
+#define ps(x, y) fixed << setprecision(y) << x
+#define w(x)  \
+    int x;    \
+    cin >> x; \
+    while (x--)
+#define all(x) (x).begin(), (x).end()
+#define sortall(x) sort(all(x))
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define REP(i, k, n) for (int i = k; i < n; ++i)
+#define REPR(i, k, n) for (int i = k; i > n; --i)
+#define pb push_back
+#define mp make_pair
+#define sz(v) (int)v.size()
+#define pii pair<int, int>
+#define vi vector<int>
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+void pgsolve()
+{
+    ll n, k;
+    cin >> n >> k;
+    vi v(n);
+    rep(i, n)
+    {
+        cin >> v[i];
+    }
+    int i = 0, j = n - 1, sum = 0, count = 0;
+    while (i < j)
+    {
+        if (v[i] != v[j])
+        {
+            sum += abs(v[i] - v[j]);
+            count++;
+        }
+
+        i++;
+        j--;
+    }
+
+    if (sum == 0)
+    {
+        yes;
+    }
+    else if (k == n)
+    {
+        no;
+    }
+    else if (k % 2)
+    {
+        yes;
+    }
+    else if (n % 2)
+    {
+        yes;
+    }
+    else
+    {
+        if (sum % 2 == 0)
+        {
+            yes;
+        }
+        else
+        {
+            no;
+        }
+    }
+}
+
+int32_t main()
+
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    auto start1 = high_resolution_clock::now();
+
+#ifdef __GNUC__
+    freopen("Error.txt", "w", stderr);
+#endif
+
+    w(t)
+    {
+        pgsolve();
+    }
+    auto stop1 = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifdef __GNUC__
+    cerr << "\n Time: " << duration.count() / 1000 << " ms" << endl;
+#endif
+    return 0;
+}
