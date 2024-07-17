d0887054e43fed7b5a81e3a463a71f2e937edfce
paras <parasguglani1@gmail.com>
2024-07-17 22:57:21 +0530

    starters 143

---
+++ Codechef/starters/starters143/Binary_Conversion.cpp
@@ -0,0 +1,143 @@
+#include <bits/stdc++.h>
+using namespace std;
+using namespace chrono;
+
+#define ff first
+#define ss second
+#define int long long
+using ll = long long;
+#define setbits(x) __builtin_popcountll(x)
+#define zrobits(x) __builtin_ctzll(x)
+#define mod 1000000007
+#define inf 1e18
+#define PI 3.1415926535897932384626
+#define ps(x, y) fixed << setprecision(y) << x
+#define w(x)  \
+    int x;    \
+    cin >> x; \
+    while (x--)
+#define all(x) (x).begin(), (x).end()
+#define sortall(x) sort(all(x))
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define REP(i, k, n) for (int i = k; i < n; ++i)
+#define REPR(i, k, n) for (int i = k; i > n; --i)
+#define pb push_back
+#define mp make_pair
+#define sz(v) (int)v.size()
+#define pii pair<int, int>
+#define vi vector<int>
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+vector<pair<int, int>> moves = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+
+void pgsolve()
+{
+    int n, k;
+    cin >> n >> k;
+    string s, t;
+    cin >> s >> t;
+    int minSwaps = 0;
+    int cntdiff = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int diff = s[i] != t[i];
+        minSwaps += (diff);
+        if (diff)
+        {
+            cntdiff += s[i] - '0';
+        }
+    }
+    // cnt1
+    int cnts = 0, cntt = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (s[i] == '1')
+        {
+            cnts++;
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (t[i] == '1')
+        {
+            cntt++;
+        }
+    }
+    if (cnts != cntt)
+    {
+        no;
+        return;
+    }
+    if (n == 2)
+    {
+        if (s != t)
+        {
+            if (k % 2)
+            {
+                yes
+            }
+            else
+            {
+                no;
+            }
+        }
+        else
+        {
+            if (k % 2 && (s=="01" ||s=="10"))
+            {
+                no;
+            }
+            else
+            {
+                yes;
+            }
+        }
+        return;
+    }
+
+    // if (minSwaps % 2)
+    // {
+    //     minSwaps++;
+    // }
+    // minSwaps /= 2;
+    if(minSwaps==cntdiff*2 &&cntdiff<=k)
+    {
+        yes
+    }
+    else
+    {
+        no;
+    }
+
+    // if (minSwaps > k)
+    // {
+    //     no;
+    //     return;
+    // }
+
+    // yes;
+}
+
+int32_t main()
+
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    auto start1 = high_resolution_clock::now();
+
+#ifdef __GNUC__
+    freopen("Error.txt", "w", stderr);
+#endif
+
+    w(t)
+    {
+        pgsolve();
+    }
+    auto stop1 = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifdef __GNUC__
+    cerr << "\n Time: " << duration.count() / 1000 << " ms" << endl;
+#endif
+    return 0;
+}
